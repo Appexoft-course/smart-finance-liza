d67567b7deb74360a48032a21e54188490299f4d
galaburdal <115738018+galaburdal@users.noreply.github.com>
2026-04-25 17:49:30 +0300

    Delete backend/app/schemas/__init__.py

--- backend/app/schemas/__init__.py
+++
@@ -1,4 +0,0 @@
-from .user import UserCreate, UserResponse
-from .category import CategoryCreate, CategoryResponse
-from .transaction import TransactionCreate, TransactionResponse
-from .transaction import TransactionCreate, TransactionResponse, TransactionUpdate
